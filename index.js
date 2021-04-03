455c7f153abcdc1d438061e6ae4afed0cd4a5475
mehmet-cloud <mehmetbozdag155@gmail.com>
2021-04-02 23:00:56 -0400

    Completed Basic Control Flow Lab

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet ( Value){
+if (Value <= 400) {
+return `This one is on me!` }
+if (Value > 2000 & Value < 2500) {
+  return `I will gladly take your thirty bucks.` ; }
+if (Value > 2500) {
+return "No can do." }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+
+
+function ternaryCheckCity(city){
+if (city === "NYC"){
+  return 'Ok, sounds good.'
+}
+else{
+  return 'No go.'
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
 }
+
+function switchOnCharmFromTip(tip){
+  if (tip === "generous"){
+    return 'Thank you so much.'
+  }
+  if (tip === "not as generous"){
+    return 'Thank you.'}
+
+  else {
+    return 'Bye.'
+  }  
+  }
+
+
+
